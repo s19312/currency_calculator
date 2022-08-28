7e7790b764e58e61e779377dbed37cc4be7f8720
Roman Fedorchenko <s19312@pjwstk.edu.pl>
2022-08-28 10:19:55 +0200

    Implemented main class

--- CurrencyCalculator/src/main/fedorchenko/currency_calculator/Main.java
+++ CurrencyCalculator/src/main/fedorchenko/currency_calculator/Main.java
@@ -1,8 +1,25 @@
 package main.fedorchenko.currency_calculator;
 
+import java.util.Scanner;
+
 public class Main {
 
-        public static void main(String[] args){
+    public static void main(String[] args) {
+        TargetCurrency targetCurrency;
+        EURCurrencyCalculator calculator;
 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Insert amount of EUR: ");
+        double eurAmount = Double.parseDouble(scanner.nextLine());
+
+        System.out.println("Provide target currency: ");
+        try {
+            targetCurrency = TargetCurrency.valueOf(scanner.nextLine());
+            calculator = new EURCurrencyCalculator(eurAmount, targetCurrency);
+            System.out.println(calculator.getEurAmount() + "EUR -> " + targetCurrency + " = " + calculator.calculate());
+        } catch (IllegalArgumentException ex) {
+            System.out.println("Such target currency does not exists!");
         }
+    }
 }
